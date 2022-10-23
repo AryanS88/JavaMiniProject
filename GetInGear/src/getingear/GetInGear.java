e381bc128efc3801f6c2f667313de5fb4f215acb
akshayrathod205 <108801502+akshayrathod205@users.noreply.github.com>
2022-10-23 18:18:23 +0530

    Update GetInGear.java

--- GetInGear/src/getingear/GetInGear.java
+++ GetInGear/src/getingear/GetInGear.java
@@ -15,12 +15,31 @@ import javafx.stage.Stage;
  * @author deep2
  */
 public class GetInGear extends Application {
-    
+
+    private double x = 0;
+    private double y = 0;
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
+        
+        root.setOnMousePressed((MouseEvent event) ->{
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+        
+        root.setOnMouseDragged((MouseEvent event) ->{
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+            stage.setOpacity(.8);
+        });
+        
+        root.setOnMouseReleased((MouseEvent event) ->{
+            stage.setOpacity(1);
+        });
+        
+        root.setOnMouse
         
         stage.setScene(scene);
         stage.show();
